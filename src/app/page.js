d0ead3990514833f80cec35f41e0cad49f28b610
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-12-09 21:41:52 +0000

    Adding basic info

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>HELLO WORLD DEV</h1>
+    <div className="h-screen flex flex-col items-center justify-center gap-5">
+        <Image src="/profile.png" alt="Logo" width={100} height={100} className="rounded-full mb-10" />
+        <h1 className="text-6xl font-extrabold">José Francisco</h1>
+        <h3>Student at Instituto Superior de Engenharia do Porto</h3>
+        <p>Aspiring Software Engineer / DevSecOps Engineer</p>
+        <div className="flex flex-row gap-x-4">
+            <a href="https://github.com/josefrancisco17"><Image src="/github.svg" alt="Github" width={30} height={30} className="rounded-full" /></a>
+            <a href="https://www.linkedin.com/in/josefrancisco17/">LinkedIn</a>
+            <a href="mailto:josefrancisco17@gmail.com">Email</a>
+        </div>
     </div>
   );
 }
